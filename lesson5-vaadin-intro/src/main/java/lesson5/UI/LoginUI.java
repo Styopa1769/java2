5f6634c696a65ce99c1c13799f3973918eaac8b2
Stepan Shcherbakov <shc-stepan@yandex.ru>
2020-04-10 16:57:17 +0300

    добавил логин

---
+++ lesson5-vaadin-intro/src/main/java/lesson5/UI/LoginUI.java
@@ -0,0 +1,23 @@
+package lesson5.UI;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.LoginForm;
+import com.vaadin.ui.UI;
+
+@SpringUI(path = "/login")
+public class LoginUI extends UI {
+    @Override
+    protected void init(VaadinRequest request) {
+        LoginForm loginForm = new LoginForm();
+
+        loginForm.addLoginListener(event -> {
+            String name = event.getLoginParameter("username");
+            String password = event.getLoginParameter("password");
+
+            //залогиньте пользователя
+        });
+        
+        setContent(loginForm);
+    }
+}
